ba68e4f2faf2115a3e2b4814bab58cdec5f3a14b
dominiclet <dominic.let@hotmail.com>
2022-01-07 11:57:05 +0800

    Improved pairing system

--- server/dist/index.js
+++ server/dist/index.js
@@ -23,27 +23,54 @@ app.get('/', (_, res) => {
     res.status(200).send("Hello");
 });
 io.on("connection", (socket) => {
-    console.log("New connection by", socket.id);
-    if (pairingQueue.length == 0)
-        pairingQueue.push(socket.id);
-    else {
-        const matched = pairingQueue.shift();
-        // Emit pair messages 
-        socket.emit("pair", matched);
-        io.to(matched).emit("pair", socket.id);
-        console.log(`Paired ${socket.id} with ${matched}`);
-        // Set to listen for each other's sockets 
-        const otherSocket = io.sockets.sockets.get(matched);
-        socket.on("chat", (msg) => {
-            io.to(matched).to(socket.id).emit("chat", msg);
-        });
-        otherSocket.on("chat", (msg) => {
-            io.to(socket.id).to(matched).emit("chat", msg);
-        });
-    }
+    console.log(`New connection by ${socket.id}, adding to pairing queue...`);
+    pairingQueue.push(socket.id);
     socket.on("disconnect", (reason) => {
         console.log(`${socket.id} has disconnected (Reason: ${reason})`);
+        if (pairingQueue[0] == socket.id)
+            pairingQueue.pop();
     });
 });
+// Pairing loop
+setInterval(() => {
+    if (pairingQueue.length <= 1)
+        return;
+    const match1Id = pairingQueue.shift();
+    const match2Id = pairingQueue.shift();
+    // Check if sockets are still connected
+    const match1 = io.sockets.sockets.get(match1Id);
+    const match2 = io.sockets.sockets.get(match2Id);
+    if (!match1 && match2) {
+        pairingQueue.push(match2Id);
+        return;
+    }
+    else if (!match2 && match1) {
+        pairingQueue.push(match1Id);
+        return;
+    }
+    else if (!match1 && match2) {
+        return;
+    }
+    // Emit pair messages
+    match1.emit("pair", match2Id);
+    match2.emit("pair", match1Id);
+    console.log(`Paired ${match1Id} with ${match2Id}`);
+    // Listen to each other's chats
+    match1.on("chat", (msg) => {
+        io.to(match2Id).to(match1Id).emit("chat", msg);
+    });
+    match2.on("chat", (msg) => {
+        io.to(match1Id).to(match2Id).emit("chat", msg);
+    });
+    // Handle disconnect events
+    match1.on("disconnect", () => {
+        io.to(match2Id).emit("chat", `${match1Id} has disconnected, finding you a new chat partner...`);
+        pairingQueue.push(match2Id);
+    });
+    match2.on("disconnect", () => {
+        io.to(match1Id).emit("chat", `${match2Id} has disconnected, finding you a new chat partner...`);
+        pairingQueue.push(match1Id);
+    });
+}, 3000);
 httpServer.listen(port, () => console.log(`Running on port ${port}`));
 //# sourceMappingURL=index.js.map
