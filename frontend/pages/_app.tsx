2a9ef4dac7ef98631b324d8a998a9efb28ca4433
dominiclet <dominic.let@hotmail.com>
2022-01-13 22:02:57 +0800

    Redirect from http to https

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,8 +1,18 @@
 import { AppProps } from 'next/app'
+import { useEffect } from 'react';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  // Check protocol
+  useEffect(() => {
+    if (window.location.protocol == 'http:' && window.location.hostname != 'localhost') {
+      window.location.href = window.location.href.replace('http:', 'https:');
+    }
+  },[]);
+
+  return (
+    <Component {...pageProps} />
+  );
 }
 
 export default MyApp
